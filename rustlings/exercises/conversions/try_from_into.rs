9e4e6b93b3a24e63215a32f0e5c078461f5c5c0b
ckafrouni <ckafrouni@proton.me>
2024-01-16 00:51:35 +0100

    Finished Rustlings !!

--- rustlings/exercises/conversions/try_from_into.rs
+++ rustlings/exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,19 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let tuple = &(
+            u8::try_from(tuple.0),
+            u8::try_from(tuple.1),
+            u8::try_from(tuple.2),
+        );
+        match tuple {
+            (Ok(red), Ok(green), Ok(blue)) => Ok(Color {
+                red: *red,
+                green: *green,
+                blue: *blue,
+            }),
+            _ => Err(IntoColorError::IntConversion),
+        }
     }
 }
 
@@ -48,6 +59,19 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let tuple = &(
+            u8::try_from(arr[0]),
+            u8::try_from(arr[1]),
+            u8::try_from(arr[2]),
+        );
+        match tuple {
+            (Ok(red), Ok(green), Ok(blue)) => Ok(Color {
+                red: *red,
+                green: *green,
+                blue: *blue,
+            }),
+            _ => Err(IntoColorError::IntConversion),
+        }
     }
 }
 
@@ -55,6 +79,23 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let tuple = &(
+            u8::try_from(slice[0]),
+            u8::try_from(slice[1]),
+            u8::try_from(slice[2]),
+        );
+        match tuple {
+            (Ok(red), Ok(green), Ok(blue)) => Ok(Color {
+                red: *red,
+                green: *green,
+                blue: *blue,
+            }),
+            _ => Err(IntoColorError::IntConversion),
+        }
     }
 }
 
--- rustlings/exercises/quiz2.rs
+++ rustlings/exercises/quiz2.rs
@@ -19,7 +19,7 @@
 // - The output element is going to be a Vector of strings.
 //
 // No hints this time!
-
+ 
 pub enum Command {
     Uppercase,
     Trim,
